8988c7b657dfe5d8a26fa776a98873b4b0f904a5
Achut Cherukuri <Achut.Cherukuri@primeroedge.com>
2019-02-19 11:13:28 -0600

    added auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire'
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
+
+// Services
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -13,9 +17,10 @@ import { environment } from 'src/environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
